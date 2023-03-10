4e53029f0b1203cee555003d105998b78ff0e6df
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-10 22:57:00 +0100

    Restore high-contrast themes

--- lib/src/themes/high_contrast.dart
+++ lib/src/themes/high_contrast.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/src/themes/common_themes.dart';
+import 'package:yaru_colors/yaru_colors.dart';
 
-final yaruHighContrastLight = createYaruLightTheme(
-  primaryColor: Colors.black,
+final yaruHighContrastLight = createYaruTheme(
+  colorScheme: const ColorScheme.highContrastLight(
+    primary: Colors.black,
+    secondary: Colors.black,
+    onSecondary: Colors.white,
+    error: YaruColors.error,
+  ),
 );
 
-final yaruHighContrastDark = createYaruDarkTheme(
-  primaryColor: Colors.white,
-  highContrast: true,
+final yaruHighContrastDark = createYaruTheme(
+  colorScheme: const ColorScheme.highContrastDark(
+    primary: Colors.white,
+    secondary: Colors.white,
+    onSecondary: Colors.black,
+    error: YaruColors.error,
+  ),
   elevatedButtonTextColor: Colors.black,
 );
